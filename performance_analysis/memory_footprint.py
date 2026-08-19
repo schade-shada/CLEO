d3494011ab0460c9c822c37bc821067945833bee
Harshada Balasubramanian <harshada.balasubramanian@mpimet.mpg.de>
2026-08-19 10:24:59 +0200

    chore: draft 1 of mental model

---
+++ performance_analysis/memory_footprint.py
@@ -0,0 +1,47 @@
+BYTES_PER_TYPE = {
+    "int32": 4,
+    "int64": 8,
+    "float32": 4,
+    "float64": 8,
+    "pointer": 8,
+}
+
+
+SD_MEMORY_FOOTPRINT = {
+    "sd-gbx-index": ("int64", 1),
+    "multiplicity": ("int64", 1),
+    "spatial_coord": ("float64", 3),
+    "Attr": ("float64", 2),
+}
+
+
+GBX_MEMORY_FOOTPRINT = {
+    "gbx_index": ("int64", 1),
+    "states": ("float64", 4),
+    "view": ("pointer", 2),
+}
+
+
+def memory_footprint(footprint, types=BYTES_PER_TYPE):
+    return sum(
+        count * BYTES_PER_TYPE[types.get(dtype, dtype)]
+        for dtype, count in footprint.values()
+    )
+
+
+def max_sd_per_gbx(vram_bytes, n_gbx):
+    """
+    Calculate the maximum number of superdroplets per gridbox
+    that can fit in the available VRAM.
+    """
+    sd_bytes = memory_footprint(SD_MEMORY_FOOTPRINT)
+    gbx_bytes = memory_footprint(GBX_MEMORY_FOOTPRINT)
+
+    gbx_memory = n_gbx * gbx_bytes
+
+    if gbx_memory >= vram_bytes:
+        raise ValueError("The gridbox memory footprint exceeds the available VRAM.")
+
+    available_sd_memory = vram_bytes - gbx_memory
+
+    return int(available_sd_memory / (sd_bytes * n_gbx))
