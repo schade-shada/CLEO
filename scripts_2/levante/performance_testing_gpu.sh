1a079fd1fb83dbdc6f47670ce7a11e176f87da0c
Harshada Balasubramanian <harshada.balasubramanian@mpimet.mpg.de>
2026-08-24 16:26:47 +0200

    chore: include nvtx zone for roofline

--- scripts_2/levante/performance_testing_gpu.sh
+++ scripts_2/levante/performance_testing_gpu.sh
@@ -22,15 +22,23 @@ export KOKKOS_TOOLS_LIBS=/home/m/m301159/CLEO_profiling/kokkos-tools/profiling/n
 source "${CLEO_PATH2CLEO}/scripts_2/common/check_inputs.sh"
 check_args_not_empty "${CLEO_PYTHON}" "${CLEO_YACYAXTROOT}"
 
-nsys_output="/scratch/m/m301159/nsys_output"
-# export CLEO_PYTHON="nsys profile \
-#   -t cuda,mpi,nvtx,osrt,cublas \
+# nsys_output="/scratch/m/m301159/nsys_output"
+# export NSYS_PREFIX="nsys profile \
+#   -t nvtx \
 #   --gpu-metrics-device=all \
+#   --gpu-metrics-frequency=1000 \
 #   --cuda-memory-usage=true \
 #   --stats=true \
-#   -o ${nsys_output} \
-#   ${CLEO_PYTHON}"
+#   -o ${nsys_output}/cleo_nsys_as2017"
 
-export NSYS_PREFIX="nsys profile -t cuda,mpi,nvtx,osrt,cublas --gpu-metrics-device=all --cuda-memory-usage=true --stats=true -o ${nsys_output}"
+roofline_output="/scratch/m/m301159/nsys_output"
+export NSYS_PREFIX="ncu \
+  --nvtx \
+  --nvtx-include "condensation/" \
+  --section SpeedOfLight_RooflineChart \
+  --section MemoryWorkloadAnalysis \
+  --section MemoryWorkloadAnalysis_Chart \
+  --target-processes all \
+    -o ${roofline_output}/cleo_constthermo_test_roofline"
 
 "${CLEO_PATH2CLEO}/scripts_2/levante/build_compile_run_plot_cleo.sh" constthermo2d cuda gcc "${CLEO_PATH2CLEO}"
