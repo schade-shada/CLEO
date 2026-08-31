ac806427c5b510ac13aa9635b7f3f6565cdaeb2c
Harshada Balasubramanian <harshada.bala@gmail.com>
2026-08-31 10:51:32 +0200

    feat: update gbx and superdroplet footprint

--- performance_analysis/memory_footprint.py
+++ performance_analysis/memory_footprint.py
@@ -6,28 +6,42 @@ BYTES_PER_TYPE = {
     "pointer": 8,
 }
 
+SD_ATTR_MEMORY_FOOTPRINT = {
+    "solute-properties": ("float64", 3),
+    "multiplicity": ("int64", 1),
+    "Attr": ("float64", 2), #radius and mass
+}
 
 SD_MEMORY_FOOTPRINT = {
     "sd-gbx-index": ("int64", 1),
-    "multiplicity": ("int64", 1),
     "spatial_coord": ("float64", 3),
-    "Attr": ("float64", 2),
+    **SD_ATTR_MEMORY_FOOTPRINT,
 }
 
+GBX_INDEX_MEMORY_FOOTPRINT = {
+    "index": ("int64", 1),
+}
+
+SUPERS_IN_GBX_MEMORY_FOOTPRINT = {
+    "index": ("int64", 1),
+    "references": ("pointer", 2),
+}
+
+STATE_MEMORY_FOOTPRINT = {
+    "states": ("float64", 8), #volume, press, temp, qvap, qcond, wvel, uvel, vvel
+}
 
 GBX_MEMORY_FOOTPRINT = {
-    "gbx_index": ("int64", 1),
-    "states": ("float64", 4),
-    "view": ("pointer", 2),
+    **GBX_INDEX_MEMORY_FOOTPRINT,
+    **SUPERS_IN_GBX_MEMORY_FOOTPRINT,
+    **STATE_MEMORY_FOOTPRINT,
 }
-
 
 def memory_footprint(footprint, types=BYTES_PER_TYPE):
     return sum(
         count * BYTES_PER_TYPE[types.get(dtype, dtype)]
         for dtype, count in footprint.values()
     )
-
 
 def max_sd_per_gbx(vram_bytes, n_gbx):
     """
